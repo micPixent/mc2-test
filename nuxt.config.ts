f499a6ff97d5b28bffe87a9517bad265e40ff475
micPixent <162548204+micPixent@users.noreply.github.com>
2024-07-27 20:59:05 +0800

    chore: husky

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,11 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-	compatibilityDate: "2024-04-03",
-	devtools: { enabled: true },
-	modules: ["@nuxt/eslint"],
-	eslint: {
-		checker: {
-			configType: "eslintrc",
-		},
-	},
+  compatibilityDate: "2024-04-03",
+  devtools: { enabled: true },
+  modules: ["@nuxt/eslint"],
 });
