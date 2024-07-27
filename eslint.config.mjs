7a5c1b3b1834a9aa948e7e456b73d452d80f7d01
micPixent <162548204+micPixent@users.noreply.github.com>
2024-07-27 17:47:18 +0800

    chore: eslint setup

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,8 +1,8 @@
+// @ts-check
 import withNuxt from './.nuxt/eslint.config.mjs'
 
 export default withNuxt({
-    files: ['**/*.ts', '**/*.tsx'],
     rules: {
-        'no-console': 'off' // allow console.log in TypeScript files
+        "no-console": 'warn'
     }
-}, {})
+})
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,11 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-    compatibilityDate: "2024-04-03",
-    devtools: { enabled: true },
-    modules: ["@nuxt/eslint"]
+	compatibilityDate: "2024-04-03",
+	devtools: { enabled: true },
+	modules: ["@nuxt/eslint"],
+	eslint: {
+		checker: {
+			configType: "eslintrc",
+		},
+	},
 });
