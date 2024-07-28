00543cb389123bc19038449254f8fa62d54a4bde
micPixent <162548204+micPixent@users.noreply.github.com>
2024-07-28 14:45:11 +0800

    chore: utils setup

---
+++ config/AppConfig.ts
@@ -0,0 +1,24 @@
+const AppConfig = {
+  cryptoCurrencyOption: [
+    {
+      label: "ETHEREUM",
+      value: "BINANCE:ETHUSD",
+    },
+    {
+      label: "BITCOIN",
+      value: "BINANCE:BTCUSD",
+    },
+    {
+      label: "BINANCE",
+      value: "BINANCE:BNBUSD",
+    },
+  ],
+  tradingChartStaticConfig: {
+    autosize: true,
+    hide_top_toolbar: true,
+    timezone: "Etc/UTC",
+    locale: "en",
+  },
+};
+
+export default AppConfig;
---
+++ utils/classNames.ts
@@ -0,0 +1,3 @@
+export const classNames = (...classes: Array<string | undefined | null | false>): string => {
+  return classes.filter(Boolean).join(" ");
+};
