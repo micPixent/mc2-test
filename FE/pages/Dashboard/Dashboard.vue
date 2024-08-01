029e6fc5869d33fa11b31247fdb8b5d9248a1fc7
micPixent <162548204+micPixent@users.noreply.github.com>
2024-08-01 16:30:17 +0800

    fix:

--- FE/pages/Dashboard/Dashboard.vue
+++ FE/pages/Dashboard/Dashboard.vue
@@ -17,7 +17,7 @@
           </div>
         </Block>
 
-        <TradingChart :selectedSymbol="selectedOption" />
+        <TradingChart :selectedSymbol="selectedOption" :watchlist="userWatchlists" />
       </Containers>
     </div>
 
